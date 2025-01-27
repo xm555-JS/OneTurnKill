2290660ffd3b398960911c34eaa337b2da3a3d5d
xm555-JS <xm555@naver.com>
2025-01-27 18:37:01 +0900

    screen resolution

---
+++ OneTurnKill/Assets/Script/Camera/Camera.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    void Start()
+    {
+        SetResoultion();
+    }
+
+    void SetResoultion()
+    {
+        int setWidth = 1920;
+        int setHeight = 1080;
+
+        int deviceWidth = Screen.width;
+        int deviceHeight = Screen.height;
+
+        //Screen.Set
+    }
+}
