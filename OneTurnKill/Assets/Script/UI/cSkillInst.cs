b90f0b34777464c66f0bbcd295a17a0ce982d476
xm555-JS <xm555@naver.com>
2025-02-21 16:50:51 +0900

    Shop_Skill_Gacha

--- OneTurnKill/Assets/Script/UI/cSkillInst.cs
+++ OneTurnKill/Assets/Script/UI/cSkillInst.cs
@@ -1,41 +1,107 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cInstanate : MonoBehaviour
+// test
+using UnityEngine.UI;
+
+public class cSkillInst : MonoBehaviour
 {
     // inven류의 UI에서 객체를 계속 생성할 때 
-    [Header("Ower")]
-    [SerializeField] GameObject ower;
+    [Header("Owner")]
+    [SerializeField] GameObject owner;
 
     [Header("Skill")]
-    [SerializeField] GameObject rSkill;
-    [SerializeField] GameObject srSkill;
-    [SerializeField] GameObject ssrSkill;
+    [SerializeField] GameObject[] rSkill;
+    [SerializeField] GameObject[] srSkill;
+    [SerializeField] GameObject[] ssrSkill;
+    
+    // Shop SKill UI Instantiate
+    [Header("UIOwer")]
+    [SerializeField] GameObject uiOwner;
+
+    [Header("SkillUI")]
+    [SerializeField] GameObject skillUI;
+    Image skillUIImage;
+
+    WaitForSeconds skillInstTime = new WaitForSeconds(0.2f);
+
+    // Shop SKill UI Instantiate
+    public event Action OnEnd;
 
     float[] skillProbs = { 60f, 30f, 10f };
 
+    void Awake()
+    {
+        skillUIImage = skillUI.GetComponent<Image>();
+    }
+
     public void InstanateUI()
     {
-        for (int i = 0; i < 10; i++)
-        {
-            float skill = Choice(skillProbs);
+        StartCoroutine(ISkillInst());
+    }
 
-            switch (skill)
-            {
-                case 0:
-                    GameObject rSkillObj = Instantiate(rSkill);
-                    rSkillObj.transform.SetParent(ower.transform, false);
-                    break;
-                case 1:
-                    GameObject srSkillObj = Instantiate(srSkill);
-                    srSkillObj.transform.SetParent(ower.transform, false);
-                    break;
-                case 2:
-                    GameObject ssrSkillObj = Instantiate(ssrSkill);
-                    ssrSkillObj.transform.SetParent(ower.transform, false);
-                    break;
-            }
+    IEnumerator ISkillInst()
+    {
+        yield return null;
+        inst();
+
+        yield return skillInstTime;
+        inst();
+
+        yield return skillInstTime;
+        inst();
+
+        yield return skillInstTime;
+        inst();
+
+        yield return skillInstTime;
+        inst();
+
+        // event
+        //OnEnd?.Invoke();
+    }
+
+    void inst()
+    {
+        float skill = Choice(skillProbs);
+
+        switch (skill)
+        {
+            case 0:
+                int rIndex = UnityEngine.Random.Range(0, rSkill.Length);
+                GameObject rSkillObj = Instantiate(rSkill[rIndex]);
+                rSkillObj.transform.SetParent(owner.transform, false);
+
+                // Shop SKill UI Instantiate
+                GameObject skillUIInst = Instantiate(skillUI);
+                skillUIInst.transform.SetParent(uiOwner.transform, false);
+                Image skillImage = skillUIInst.GetComponent<Image>();
+                skillImage.sprite = rSkillObj.GetComponentsInChildren<Image>()[1].sprite;
+                break;
+            case 1:
+                int srIndex = UnityEngine.Random.Range(0, srSkill.Length);
+                GameObject srSkillObj = Instantiate(srSkill[srIndex]);
+                srSkillObj.transform.SetParent(owner.transform, false);
+
+                // Shop SKill UI Instantiate
+                GameObject srSkillUIInst = Instantiate(skillUI);
+                srSkillUIInst.transform.SetParent(uiOwner.transform, false);
+                Image srSkillImage = srSkillUIInst.GetComponent<Image>();
+                srSkillImage.sprite = srSkillObj.GetComponentsInChildren<Image>()[1].sprite;
+                break;
+            case 2:
+                int ssrIndex = UnityEngine.Random.Range(0, ssrSkill.Length);
+                GameObject ssrSkillObj = Instantiate(ssrSkill[ssrIndex]);
+                ssrSkillObj.transform.SetParent(owner.transform, false);
+
+                // Shop SKill UI Instantiate
+                GameObject ssrSkillUIInst = Instantiate(skillUI);
+                ssrSkillUIInst.transform.SetParent(uiOwner.transform, false);
+                Image ssrSkillImage = ssrSkillUIInst.GetComponent<Image>();
+                ssrSkillImage.sprite = ssrSkillObj.GetComponentsInChildren<Image>()[1].sprite;
+                break;
         }
     }
 
@@ -45,7 +111,7 @@ public class cInstanate : MonoBehaviour
         foreach (var elem in probs)
             total += elem;
 
-        float randomPoint = Random.value * total;
+        float randomPoint = UnityEngine.Random.value * total;
 
         for (int i = 0; i < probs.Length; i++)
         {
