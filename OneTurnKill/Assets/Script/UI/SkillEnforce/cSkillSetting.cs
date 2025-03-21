90a59fdcab9caed08ec6f7a51a7bc4dc5a6cafb8
xm555-JS <xm555@naver.com>
2025-03-21 16:07:09 +0900

    Update cSkillSetting

--- OneTurnKill/Assets/Script/UI/SkillEnforce/cSkillSetting.cs
+++ OneTurnKill/Assets/Script/UI/SkillEnforce/cSkillSetting.cs
@@ -46,75 +46,68 @@ public class cSkillSetting : MonoBehaviour
     {
         DuplicateCheck(btn, skillData);
 
+        // 버튼 셋팅
         btn.GetComponent<Image>().sprite = skillData.skillSprite;
         buttonPair[btn].GetComponent<cPlayerSkillButton>().SkillName = skillData.skillName;
         buttonPair[btn].GetComponent<cPlayerSkillButton>().ChangeImage(skillData.skillSprite);
 
-        // 정보 저장
-        if (buttonID.ContainsKey(btn))
-            buttonID[btn] = skillData;
-        else
-            buttonID.Add(btn, skillData);
+        ButtonIDRegister(btn, skillData);
     }
 
     void DuplicateCheck(Button btn, SkillData skillData)
     {
-        // 만약 지금 설정하려는 스킬이 다른 칸에 설정되어 있다면
-        // 스킬을 서로 바꾼다.
+        // 스킬 셋팅 중복 체크
         foreach (var otherBtn in btnArray)
         {
-            if (buttonID.ContainsKey(otherBtn))
+            if (otherBtn == btn)
+                continue;
+
+            if (buttonID.ContainsKey(otherBtn) && skillData == buttonID[otherBtn]) // 셋팅된 스킬중에 중복된 스킬이 있다면 스킬 버튼을 바꾼다
             {
-                // 만약에 btn에 설정하려는 스킬(skillData)이 otherBtn와 같다면
-                if (skillData == buttonID[otherBtn])
+                if (buttonID.ContainsKey(btn) == false) // 바꾸려는 버튼이 빈칸일 때
                 {
-                    // btn이 empty일 때
-                    // otherBtn을 empty로 만든다.
-                    if (buttonID.ContainsKey(btn) == false)
-                    {
-                        otherBtn.GetComponent<Image>().sprite = btn.GetComponent<Image>().sprite;
-                        buttonPair[otherBtn].GetComponent<cPlayerSkillButton>().SkillName = "";
-                        buttonPair[otherBtn].GetComponent<cPlayerSkillButton>().ChangeImage(defaultBtnSprite);
-                        break;
-                    }
-                    // btn이 설정되어있을 때
-                    // otherBtn을 btn에 있는 스킬로 만든다
-                    else
-                    {
-                        otherBtn.GetComponent<Image>().sprite = btn.GetComponent<Image>().sprite;
-                        SkillData preData = buttonID[btn];
-                        buttonPair[otherBtn].GetComponent<cPlayerSkillButton>().SkillName = preData.skillName;
-                        buttonPair[otherBtn].GetComponent<cPlayerSkillButton>().ChangeImage(preData.skillSprite);
-                        return;
-                        // 이거 원래 같은 스킬을 가지고있는 버튼 하나만 바꿔야하는데
-                        // 모든 다른 버튼을 바꿔버려서 고쳐야함.
-                    }
+                    ChangeButtonInfo(otherBtn, btn);
+                    break;
+                }
+                else // 빈칸이 아닐때
+                {
+                    SkillData preData = buttonID[btn];
+                    ChangeButtonInfo(otherBtn, btn, preData);
+                    return;
                 }
             }
         }
     }
+
+    void ChangeButtonInfo(Button otherbtn, Button btn, SkillData skillData = null)  // 스킬 버튼 바꾸는 함수
+    {
+        if (skillData == null)
+        {
+            otherbtn.GetComponent<Image>().sprite = btn.GetComponent<Image>().sprite;
+            buttonPair[otherbtn].GetComponent<cPlayerSkillButton>().SkillName = "";
+            buttonPair[otherbtn].GetComponent<cPlayerSkillButton>().ChangeImage(defaultBtnSprite);
+
+            ButtonIDRemove(otherbtn);
+        }
+        else
+        {
+            otherbtn.GetComponent<Image>().sprite = btn.GetComponent<Image>().sprite;
+            buttonPair[otherbtn].GetComponent<cPlayerSkillButton>().SkillName = skillData.skillName;
+            buttonPair[otherbtn].GetComponent<cPlayerSkillButton>().ChangeImage(skillData.skillSprite);
+
+            ButtonIDChange(otherbtn, skillData);
+        }
+    }
+
+    void ButtonIDRegister(Button btn, SkillData data)   // 버튼 정보 등록
+    {
+        if (buttonID.ContainsKey(btn))
+            buttonID[btn] = data;
+        else
+            buttonID.Add(btn, data);
+    }
+
+    void ButtonIDRemove(Button btn) { buttonID.Remove(btn); }   // 버튼 정보 삭제
+
+    void ButtonIDChange(Button btn, SkillData data) { buttonID[btn] = data; }   // 버튼 정보 바꾸기
 }
-
-
-//if (otherBtn == btn)    // 설정하려는 버튼은 제외
-//    continue;
-////if (buttonID.ContainsKey(otherBtn) == false)
-////    continue;
-
-//if (skillData == buttonID[otherBtn]) // 설정하려는 버튼은 제외하고 나머지 두 버튼 중 같은 스킬이있다면
-//{
-//    // btn의 셋팅을 skillBtn로 옮기자
-//    otherBtn.GetComponent<Image>().sprite = btn.GetComponent<Image>().sprite;
-
-//    if (buttonID.ContainsKey(otherBtn))
-//    {
-//        SkillData preData = buttonID[btn];
-//        buttonPair[btn].GetComponent<cPlayerSkillButton>().SkillName = preData.skillName;
-//        buttonPair[btn].GetComponent<cPlayerSkillButton>().ChangeImage(preData.skillSprite);
-//    }
-//    else
-//    {
-//        buttonPair[btn].GetComponent<cPlayerSkillButton>().SkillName = "";
-//        buttonPair[btn].GetComponent<cPlayerSkillButton>().ChangeImage(defaultBtnSprite);
-//    }
-//}
