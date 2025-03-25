b0af82c891d26e14a33f2bc4050863737eeee113
xm555-JS <xm555@naver.com>
2025-03-25 14:02:30 +0900

    spend_money

--- OneTurnKill/Assets/Script/Player/Stats/Stat_UI/View/Stats/cPlayerStatsUI.cs
+++ OneTurnKill/Assets/Script/Player/Stats/Stat_UI/View/Stats/cPlayerStatsUI.cs
@@ -7,9 +7,14 @@ public abstract class cPlayerStatsUI : MonoBehaviour
 {
     // View
     protected Button statButton;
+    protected Text goldText;
     protected Text titleText;
     protected Text StatText;
     protected Text nextStatText;
+
+    // default price
+    protected int price = 1000;
+    protected int priceIncreaseAmount = 100;
 
     // present
     protected cPlayerStatsPresent present;
@@ -19,6 +24,7 @@ public abstract class cPlayerStatsUI : MonoBehaviour
     protected void Awake()
     {
         statButton = GetComponent<Button>();
+        goldText = GetComponentsInChildren<Text>()[0];
         titleText = GetComponentsInChildren<Text>()[1];
         StatText = GetComponentsInChildren<Text>()[2];
         nextStatText = GetComponentsInChildren<Text>()[3];
@@ -61,6 +67,10 @@ public abstract class cPlayerStatsUI : MonoBehaviour
         
         StatText.text = statValue.ToString();
         nextStatText.text = nextStatValue.ToString();
+
+        GameManager.instance.playerCom.SpendCoin(price);
+        price += priceIncreaseAmount;
+        goldText.text = price.ToString();
     }
 
     public void UpdateCharStat(string Name,int level, float statValue, float nextStatValue)
@@ -90,5 +100,9 @@ public abstract class cPlayerStatsUI : MonoBehaviour
 
         StatText.text = statValue.ToString() + "%";
         nextStatText.text = nextStatValue.ToString() + "%";
+
+        GameManager.instance.playerCom.SpendCoin(price);
+        price += priceIncreaseAmount;
+        goldText.text = price.ToString();
     }
 }
